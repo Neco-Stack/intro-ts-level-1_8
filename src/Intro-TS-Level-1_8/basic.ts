4cc02582ca2265161010faa09d848cef97c90cf2
Neco_Stack <necoabay@googlemail.com>
2024-09-29 17:53:03 +0200

    operators2

---
+++ src/Intro-TS-Level-1_8/basic.ts
@@ -0,0 +1,78 @@
+// deklariere eine Variable udn gebe Ihr explizit ein Typ number
+let x:number = 20;
+let y:number = 30; 
+console.log(x);
+console.log(y);
+// addiere x,y und gebe das Ergebnis in der Konsole aus 
+// let a:number = x + y;
+// console.log(a);
+// // subtrahiere y von x und gebe es in der Konmsole aus 
+// let b:number = x - y; 
+// console.log(b);
+
+// let c:number = y - x; 
+// console.log(c);
+
+let d:number = x * y;
+console.log(d);
+
+let e:number = x / y; 
+console.log(e);
+
+let z:number = 10; 
+// speichere das Ergebnis als Variable ab: 
+const resultOne:number = (x * y) / z;
+console.log(resultOne);
+
+let a:number = 15;
+let b:number = 9; 
+console.log(a % b);
+
+let c:number = 20;
+const resultTwo:number = (a + b) * c;
+console.log(resultTwo);
+
+// increment operator
+a ++;
+console.log(a);
+
+// decrement operator
+b --;
+console.log(b);
+const resultThree:number = a - b;
+console.log(resultThree);
+
+// modulo operator
+console.log(resultOne % resultTwo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   <a href='./src/Intro-TS-Level-1_8/basic.html'>Intro TS</a>
+  </div>
+`
